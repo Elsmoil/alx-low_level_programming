fcd72fdc7d257df1de07ff4284955ca46f0ee2d9
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-07 12:42:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#inculde <stdio.h>
+/*
+ * main - A program that prints a line with puts function 
+ * Return: 0 (Success)
+ */
+int main(viod)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
