b400279ace2a59f5eee233f68c10b2868107f7b8
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-08 08:41:34 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+char main(void)
+{	char x = 'a';
+	char y = 'A';
+
+	while
+	{putchar(x <= a);
+		x++;
+	}
+	while
+	{	putchar(y <= A);
+		A++;
+	}
+	putchar('\n')
+		return (0);
+}
