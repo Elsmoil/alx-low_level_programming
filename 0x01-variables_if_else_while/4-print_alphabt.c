fb8413e2bb2d4c3a8df28cdb700891db602aea90
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-08 10:43:29 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,13 +3,16 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char n;
 
 	n = 'a';
 	while
-		(n <= a, n == q, e);	{
+		(n <= 'a')
+	{
+		if (n == 'e' || n == 'q')
 			putchar(n);
 			n++;
 	}
