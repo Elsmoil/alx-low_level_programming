b99d5ab54c005a0b138863c0d85e20d0dd624fe6
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-07 18:42:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
+#include<unistd.h>
 /**
- * main - Enter point
- * Return: Always(1)Success
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 		retrun(1);
 }
