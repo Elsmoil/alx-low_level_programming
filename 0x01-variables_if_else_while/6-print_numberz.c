f5dcc5f1918c0ff39d6e62c770c8a43479282a45
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-08 12:03:46 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 1;
+	for (;i <= 10;) {
+		putchar("%d", i);
+		i++;
+	}
+return (0);
+}
