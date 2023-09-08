bc6d59d8f5b22cbbe707b655f1d0c3a1d5f1c8c3
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-08 08:21:00 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entr point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a';
+
+	while
+		(n <= 'z')
+		{
+		putchar(n);
+		n++;
+		}
+	putchar('\n');
+	return (0);
+}
