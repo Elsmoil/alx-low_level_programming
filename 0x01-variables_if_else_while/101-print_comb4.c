e53ba2ef97f49de9fa4241dd08c5625f313f3e14
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-08 16:11:31 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,14 +6,18 @@
 
 int main(void)
 {
-	int digit1 = 0, digit2, digit3;
+	int digit1 = 0;
+	int digit2, digit3;
 
-	while (digit1 <= 36)
+	while (digit1 <= 9)
 	{
 		digit2 = 0;
-		while (digit2 <= 36)
+		while (digit2 <= 9)
 		{
-			if (digit1 != digit2, digit3 && digit1 < digit2, digit3)
+			if (digit1 != digit2 &&
+			digit1 < digit2 &&
+			digit2 != digit3 &&
+			digit2 < digit3)
 	{
 				putchar(digit1 + 48);
 				putchar(digit2 + 48);
