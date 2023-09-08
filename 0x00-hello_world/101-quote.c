b9fddec83dd0079f090f62115312196b615bffb9
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-07 18:34:15 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Enter point
+ * Return: Always(1)Success
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+		retrun(1);
+}
