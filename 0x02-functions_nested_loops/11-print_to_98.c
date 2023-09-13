ec2cf818b51c5e028f413c6a31abce06338f668b
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-12 19:55:43 -0700

    mmm

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,24 +8,13 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
+	int count;
 
-	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-				printf("%d, ", i);
-			else if (i == 98)
-				 printf("%d\n", i);
-		}
-	}
-		if (n >= 98)
-	{
-		for (j = n; j >= 98; j--)
-		{
-			if (j != 98)
-				printf("%d, ", j);
-		}
-	}
+	if (n > 98)
+		for (count = n; count > 98; count--)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; count++)
+			printf("d, ", count);
+	printf("98\n");
 }
