48a0cfd04411daf12c3ee44efd4665cfacb99794
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-13 14:24:54 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,8 +26,8 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-					else if (k >= 10 && k < 100)
 				}
+					if (k >= 10 && k < 100)
 					{
 						_putchar(',');
 						_putchar(' ');
@@ -47,5 +47,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-}
-								
+}			
