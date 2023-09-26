347a39039881dbcd47810c746f2230db1af57a54
Elsmoil <alsmoilalrasta999@gmail.com>
2023-09-25 17:28:56 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_chessboard - Entry point
- * @a: array
- * Return: Always 0 (Success)
+ * print_chessboard - prints chessboard.
+ *
+ * @a: rows.
+ * Return: nothing.
  */
 void print_chessboard(char (*a)[8])
 {
@@ -13,8 +14,9 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-
-		_putchar(a[i][j]);
+		{
+			_putchar(a[i][j]);
+		}
 		_putchar('\n');
 	}
 }
