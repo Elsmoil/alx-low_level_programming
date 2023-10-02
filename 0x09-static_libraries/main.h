a099c85e102793e479c28048edb600a168b7a7d9
Elsmoil <alsmoilalrasta999@gmail.com>
2023-10-02 15:27:14 -0700

    more

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,5 +21,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int _putchar(char);
+void _puts(char *c);
 
 #endif
