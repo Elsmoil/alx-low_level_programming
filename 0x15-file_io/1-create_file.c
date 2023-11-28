dbee33a9a951a4ed1636df13f289e359ab99f9f8
Elsmoil <alsmoilalrasta999@gmail.com>
2023-11-27 17:01:40 -0800

    more mff

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _strlen - return the length of a string
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fr = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_TRUSR | S_TWUSR);
+	fr = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fr == -1)
 		return (-1);
 	if (len)
